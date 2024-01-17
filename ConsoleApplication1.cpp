e44deb93f6956732852234e0ce2e903877ea58da
byp3f3 <149417004+byp3f3@users.noreply.github.com>
2024-01-17 19:14:46 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	cout << "\t\"Я помню чудное мгновенье:\n\tПередо мной явилась ты,\n\tКак мимо \bлетное виденье,\n\tКак гений чистой красоты.\"";
+
+	return 0; 
+}
+

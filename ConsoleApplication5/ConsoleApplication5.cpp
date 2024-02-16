2656615f81507e9b9a19180080b51b0512bd3a61
byp3f3 <nekrasovaanastasia790@gmail.com>
2024-02-16 12:59:31 +0300

    massiv

---
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	setlocale(0, "");
+	
+	float sum;
+	const int rows = 3, columns = 5;
+	float num[rows][columns];
+
+	for (int i = 0; i < rows; i++) {
+		cout << "Введите пять элементов строки: " << endl;
+		for (int j = 0; j < columns; j++) {
+			cin >> num[i][j];
+		}
+	}
+	for (int i = 0; i < rows; i++) {
+		sum = 0;
+		for (int j = 0; j < columns; j++) {
+			sum += num[i][j];
+		}
+		cout <<"Среднее значение строки "<< i+1 <<": " << sum / 5 << endl;
+	}
+	return 0;
+
+}
